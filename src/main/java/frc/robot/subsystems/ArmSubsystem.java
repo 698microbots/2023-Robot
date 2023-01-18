082ad6c0b0b838e4859bc39f1dfd4a9cd0749116
Alex Nie <93626002+TheSparkyBoy@users.noreply.github.com>
2023-01-18 16:03:50 -0700

    Commit for Transfering to Rahul's Computer

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,13 +5,19 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenixpro.configs.TalonFXConfiguration;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  
+  public ArmSubsystem() {
+    
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
   }
 }
