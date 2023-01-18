29097831f193ce4a6014cec80107fa397a43a365
Swagenwagen <tannedswegion@gmail.com>
2023-01-18 16:05:50 -0700

    added solenoids

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -4,6 +4,9 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -13,9 +16,31 @@ public class Claw extends SubsystemBase {
   /** Creates a new Claw. */
   private final CANSparkMax leftSpark;
   private final CANSparkMax rightSpark;
+
+  private final DoubleSolenoid clawExtension; 
   public Claw() {
     leftSpark = new CANSparkMax(Constants.leftSparkID, MotorType.kBrushless);
     rightSpark = new CANSparkMax(Constants.rightSparkID, MotorType.kBrushless);
+    leftSpark.setInverted(true);
+
+    clawExtension = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  }
+
+  public void setSpeed(double speed) {
+    leftSpark.set(speed);
+    rightSpark.set(speed);
+  }
+
+  public void setClawExtended(boolean state) {
+    if (state == true) {
+      clawExtension.set(DoubleSolenoid.Value.kForward);
+    } else {
+      clawExtension.set(DoubleSolenoid.Value.kReverse);
+    }
+  }
+
+  public void ventClaw() {
+    clawExtension.set(DoubleSolenoid.Value.kOff);
   }
 
   @Override
