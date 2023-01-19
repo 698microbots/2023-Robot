23dff7c17b7eb1a16591ff11f6ef40f59e8be24f
rkalikota22 <rkalikota22@gmail.com>
2023-01-18 17:13:23 -0700

    sending back to alex

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,5 +15,10 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+
   }
+
+  public static final int talonArmPivot = 1;
+  public static final int talonLift1 = 2;
+  public static final int talonLift2 = 3;
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,12 +7,21 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenixpro.configs.TalonFXConfiguration;
+import frc.robot.Constants;
+import com.ctre.phoenixpro.hardware.TalonFX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import edu.wpi.first.wpilibj.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.IMotorController;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
-  
+  private  final TalonFX armPivotMotor = new TalonFX(Constants.talonArmPivot);
+  private final TalonFX elevatorLiftMotor1 = new TalonFX(Constants.talonLift1);
+  private final TalonFX elevatorLiftMotor2 = new TalonFX(Constants.talonLift2);
+
   public ArmSubsystem() {
-    
+    armPivotMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
   }
 
   @Override
